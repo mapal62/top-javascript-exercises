e32258735ec8eb42fb91b4a7d762aa346c2e8ecc
pali <mapal62@gmail.com>
2022-08-03 23:37:17 +0200

    03_reverseString TDD

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let result = '';
+    for (let i = string.length; i > 0; i--) {
+        result += string[i - 1];
+    }
+    return result;
 };
 
 // Do not edit below this line
