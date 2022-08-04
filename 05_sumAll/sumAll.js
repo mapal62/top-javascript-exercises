068260010fb5d8b933e8499a9baa43e67956fe21
pali <mapal62@gmail.com>
2022-08-04 21:33:31 +0200

    05_sumAll TDD -add a test if one of the number is 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo) &&
+        numberOne >= 0 & numberTwo >= 0) {
+            return (numberOne + numberTwo) *
+            (Math.abs(numberOne - numberTwo) + 1) / 2;
+            
+    } else return 'ERROR';
 };
 
 // Do not edit below this line
