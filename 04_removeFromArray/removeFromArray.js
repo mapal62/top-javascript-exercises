a5aaeac78fc4a63b1effc2eb1357d0eca8e54bfa
pali <mapal62@gmail.com>
2022-08-04 20:18:47 +0200

    04_removeFromArray TDD -add a test for repeating element in source

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (sourceArray, ...notNeeded) {
+    for (const clear of notNeeded) {
+        while (true) {
+            const index = sourceArray.indexOf(clear);
+            if (index === -1) break;
+            sourceArray.splice(index, 1);
+        }
+    }
+    return sourceArray;
 };
 
 // Do not edit below this line
