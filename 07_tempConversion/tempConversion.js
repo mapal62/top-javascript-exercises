fe3b57f70a3192e118c4b8921f8951f007c09192
pali <mapal62@gmail.com>
2022-08-05 13:02:48 +0200

    07_tempConversion TDD

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  if ((typeof fahrenheit) !== 'number') return 'ERROR';
+  return Math.round(((fahrenheit - 32) * 5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  if ((typeof celsius) !== 'number') return 'ERROR';
+  return Math.round((celsius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
