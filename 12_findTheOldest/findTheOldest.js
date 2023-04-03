8d545149dd9f7c286154607845c660ae79a9faea
pali <mapal62@gmail.com>
2023-04-03 13:45:19 +0200

    12_findTheOldest with reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    const oldest = people.reduce(
+        (prev, current) => {
+            const prevAge = (prev.hasOwnProperty('yearOfDeath') ? prev.yearOfDeath : currentYear) - prev.yearOfBirth;
+            const currentAge = (current.hasOwnProperty('yearOfDeath') ? current.yearOfDeath : currentYear) - current.yearOfBirth;
+            return prevAge > currentAge ? prev : current;
+        }
+    )
+    return oldest;
 };
 
 // Do not edit below this line
