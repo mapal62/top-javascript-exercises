2781c3fd042c1dec2022535d53c37e3224038785
pali <mapal62@gmail.com>
2023-04-03 13:40:52 +0200

    10_fibonacci - use basic for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (arg) {
+    const steps = Number(arg);
+    if (steps < 1) return 'OOPS';
+    let prev = 0;
+    let act = 1;
+    let res = 1;
+    for (let i = 1; i < steps; i++) {
+        res = act + prev;
+        prev = act;
+        act = res;
+    }
+    return res;
 };
 
 // Do not edit below this line
