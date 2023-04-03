db8e975d18aa9270dad5745250a16d1cd7a3d0bf
pali <mapal62@gmail.com>
2023-04-03 13:39:17 +0200

    09_palindromes - string zip with regexp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (wtf) {
+    const fillers = /[ ,.!]/g;
+    const zanza = wtf.toLowerCase().replace(fillers, '');
+    const len = zanza.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (zanza[i] !== zanza[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
