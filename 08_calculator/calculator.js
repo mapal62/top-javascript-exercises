921648f2b8ce20697aaffebd2f38c309c1b7aa79
pali <mapal62@gmail.com>
2023-04-03 13:34:10 +0200

    08_calculator - basic oprations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (items) {
+  return items.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (list) {
+  return list.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  if (b === 0) return 1;
+  let result = 1;
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
